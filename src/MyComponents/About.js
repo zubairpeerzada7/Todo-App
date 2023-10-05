f41ed0daee79bc8ae86255e9911093dde4801fc2
Peerzada Zubair Ul Islam <zubairpeerzada07@gmail.com>
2023-10-05 17:08:57 +0530

    Add files via upload

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an about component 
+            <p>"Stay organized and productive with our Todo app! Easily manage your daily tasks, create to-do lists, and set priorities. Whether it's work, home, or personal tasks, our intuitive and user-friendly interface makes it a breeze to stay on top of your responsibilities. With features you can streamline your life and achieve your goals more efficiently. Start tackling your to-dos today with our Todo app and experience the joy of a clutter-free, well-organized life."</p>
+        </div>
+    )
+}
